0d96444f019dccb8ec2893e448e278a0ff79cba0
babloo-tech <babloo.career@gmail.com>
2025-09-04 15:55:11 +0530

    update url

--- backend/controllers/course-controller.js
+++ backend/controllers/course-controller.js
@@ -161,7 +161,6 @@ export const  courseDetails = async (req,res)=>{
 import Stripe from 'stripe'
 import config from "../config.js";
 const stripe=new Stripe(config.STRIPE_SECRET_KEY)
-console.log(config.STRIPE_SECRET_KEY)
 
 // 6. buy courses by course id
 export const buyCourses=async (req,res)=>{
